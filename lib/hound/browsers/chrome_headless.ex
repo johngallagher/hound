6585d7dc587b1b25098960dac6660879b34031ba
John Gallagher <john@synapticmishap.co.uk>
2018-01-06 18:47:54 +0000

    If on heroku detect the location of chrome driver

--- lib/hound/browsers/chrome_headless.ex
+++ lib/hound/browsers/chrome_headless.ex
@@ -6,6 +6,13 @@ defmodule Hound.Browser.ChromeHeadless do
   def default_user_agent, do: :chrome
 
   def default_capabilities(ua) do
-    %{chromeOptions: %{"args" => ["--user-agent=#{ua}", "--headless", "--disable-gpu"]}}        
+    %{chromeOptions: Map.merge(chrome_binary(), %{"args" => ["--user-agent=#{ua}", "--headless", "--disable-gpu"]}) }
+  end
+
+  def chrome_binary do
+    case System.get_env("GOOGLE_CHROME_SHIM") do
+      nil -> %{}
+      path -> %{ "binary" => path }
+    end
   end
 end
